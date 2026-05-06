f0dd5a7e3a315b433716d52090d827ea803525f7
seu-usuario-do-github <prado.samara@escola.pr.gov.br>
2026-05-06 09:30:32 -0300

    testes finalizados

--- mobile/app/(tabs)/tests.py
+++ mobile/app/(tabs)/tests.py
@@ -6,3 +6,23 @@ if resultado == esperado:
     print("Teste 1 (Campos Vazios): Passou")
 else:
     print(f"Teste 1 (Campos Vazios): Falhou (Recebido: '{resultado}')")
+
+
+
+resultado = agendar("12/05/2026", "15:30", "verdadeiro")
+esperado = "agendamento realizado"
+
+if resultado == esperado:
+    print("Teste 2 (Sucesso): Passou")
+else:
+    print(f"Teste 2 (Sucesso): Falhou (Recebido: '{resultado}', Esperado: '{esperado}')")
+
+
+
+resultado = agendar("10/10/2026", "25:00", "verdadeiro")
+esperado = "hora invalida"
+
+if resultado == esperado:
+    print("Teste 3 (Hora Invalida): Passou")
+else:
+    print(f"Teste 3 (Hora Invalida): Falhou (Recebido: '{resultado}', Esperado: '{esperado}')")
